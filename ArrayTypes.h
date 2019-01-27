9dc8af85c9a6b3e2e1eb79aded3e9393bcb8cc6f
Tyler Young <tyler@x-plane.com>
2019-01-27 15:17:22 -0600

    refactor (ArrayTypes): Improved readability

--- ArrayTypes.h
+++ ArrayTypes.h
@@ -21,7 +21,7 @@
 
 // NOTE: This is *not* a full implementation of the read-only portion of the std::vector API.
 //       If you drop it in to production code, you'll probably have to add stuff.
-//       The good news is, the implementation is simple, so it should be straightforward!
+//       The good news is, the implementation is simple, so doing so should be straightforward!
 //
 // NOTE ALSO: T must support std::move() semantics.
 template<class T>
@@ -51,8 +51,8 @@ public:
 	size_t size() const                           { return m_end - m_begin; }
 	bool   empty() const                          { return m_end == m_begin; }
 
-	template<class It> void replace(It begin, It end)              { replace(begin, end, end - begin); }
-	template<class It> void replace(It begin, It end, size_t size)
+	template<class RndAccessIt> void replace(RndAccessIt begin, RndAccessIt end)              { replace(begin, end, end - begin); }
+	template<class It>          void replace(It          begin, It          end, size_t size)
 	{
 		for(T * next = m_begin; next < m_end; ++next)
 		{
@@ -112,6 +112,18 @@ struct pair_sort_first_functor {
 	bool operator()(const key_type& lhs, const T& rhs) const { return func (lhs,rhs.first); }
 };
 
+template<class FwdIt>
+static size_t compute_fwd_it_dist(FwdIt begin, FwdIt end)
+{
+	size_t out = 0;
+	while(begin != end)
+	{
+		++out;
+		++begin;
+	}
+	return out;
+}
+
 template<class KeyT, class ValueT>
 class ArrayMap : public FixedArray<std::pair<KeyT, ValueT>>
 {
@@ -121,10 +133,10 @@ public:
 	typedef typename base_type::iterator       iterator;
 	typedef typename base_type::const_iterator const_iterator;
 
-	template<class TContainer>  ArrayMap(TContainer &container)               : base_type(container.begin(), container.end())      { std::sort(base_type::begin(), base_type::end()); }
-	                            ArrayMap(std::initializer_list<value_type> l) : base_type(l.begin(), l.end())                      { std::sort(base_type::begin(), base_type::end()); }
-	template<class FwdIt>       ArrayMap(FwdIt begin, FwdIt end)              : base_type(begin, end, compute_it_dist(begin, end)) { std::sort(base_type::begin(), base_type::end()); }
-	                            ArrayMap(std::map<KeyT, ValueT> &m)           : base_type(m.begin(), m.end(), m.size())            { std::sort(base_type::begin(), base_type::end()); }
+	template<class TContainer>  ArrayMap(TContainer &container)               : base_type(container.begin(), container.end())          { std::sort(base_type::begin(), base_type::end()); }
+	                            ArrayMap(std::initializer_list<value_type> l) : base_type(l.begin(), l.end())                          { std::sort(base_type::begin(), base_type::end()); }
+	template<class FwdIt>       ArrayMap(FwdIt begin, FwdIt end)              : base_type(begin, end, compute_fwd_it_dist(begin, end)) { std::sort(base_type::begin(), base_type::end()); }
+	                            ArrayMap(std::map<KeyT, ValueT> &m)           : base_type(m.begin(), m.end(), m.size())                { std::sort(base_type::begin(), base_type::end()); }
 
 	~ArrayMap()                                       { base_type::clear(); }
 
@@ -132,25 +144,13 @@ public:
 	ArrayMap & operator=(const TContainer &rhs)       { replace(rhs.begin(), rhs.end()); return *this; }
 
 	size_t         count(const KeyT &key) const       { return std::binary_search(base_type::begin(), base_type::end(), key, pair_sort_first_functor<value_type>());	}
-	iterator       find( const KeyT &key)             { return std::lower_bound(base_type::begin(), base_type::end(), key, pair_sort_first_functor<value_type>());			}
-	const_iterator find( const KeyT &key) const       { return std::lower_bound(base_type::begin(), base_type::end(), key, pair_sort_first_functor<value_type>());			}
+	iterator       find( const KeyT &key)             { auto i = std::lower_bound(base_type::begin(), base_type::end(), key, pair_sort_first_functor<value_type>()); return (i == base_type::end() || i->first != key) ? base_type::end() : i; }
+	const_iterator find( const KeyT &key) const       { auto i = std::lower_bound(base_type::begin(), base_type::end(), key, pair_sort_first_functor<value_type>()); return (i == base_type::end() || i->first != key) ? base_type::end() : i; }
 	const ValueT & at(   const KeyT &key) const       { return operator[](key);	}
-	const ValueT & operator[](const KeyT &key) const  { const_iterator i = find(key); assert(i != base_type::end()); return i->second; }
+	const ValueT & operator[](const KeyT &key) const  { assert(find(key) != base_type::end()); return find(key)->second; }
 
-	template<class It> void replace(It begin, It end) { base_type::replace(begin, end); std::sort(begin(), end()); }
-
-private:
-	template<class FwdIt>
-	static size_t compute_it_dist(FwdIt begin, FwdIt end)
-	{
-		size_t out = 0;
-		while(begin != end)
-		{
-			++out;
-			++begin;
-		}
-		return out;
-	}
+	template<class It>
+	void replace(It begin, It end) { base_type::replace(begin, end); std::sort(begin(), end()); }
 };
 
 
@@ -159,36 +159,18 @@ class ArraySet : public FixedArray<T>
 {
 public:
 	typedef FixedArray<T>                      base_type;
-	typedef typename base_type::iterator       iterator;
-	typedef typename base_type::const_iterator const_iterator;
 
-	template<class TContainer> ArraySet(TContainer &container)      : base_type(container.begin(), container.end())      { std::sort(base_type::begin(), base_type::end()); }
-	                           ArraySet(std::initializer_list<T> l) : base_type(l.begin(), l.end())                      { std::sort(base_type::begin(), base_type::end()); }
-	template<class FwdIt>      ArraySet(FwdIt begin, FwdIt end)     : base_type(begin, end, compute_it_dist(begin, end)) { std::sort(base_type::begin(), base_type::end()); }
+	template<class TContainer> ArraySet(TContainer &container)      : base_type(container.begin(), container.end())          { std::sort(base_type::begin(), base_type::end()); }
+	                           ArraySet(std::initializer_list<T> l) : base_type(l.begin(), l.end())                          { std::sort(base_type::begin(), base_type::end()); }
+	template<class FwdIt>      ArraySet(FwdIt begin, FwdIt end)     : base_type(begin, end, compute_fwd_it_dist(begin, end)) { std::sort(base_type::begin(), base_type::end()); }
 
 	template<class TContainer>
 	ArraySet & operator=(const TContainer &rhs) { replace(rhs.begin(), rhs.end()); return *this; }
 
-	iterator		find(const T &item)		  { return std::lower_bound(base_type::begin(), base_type::end(), item, pair_sort_first_functor<T>());	}
-	const_iterator	find(const T &item) const { return std::lower_bound(base_type::begin(), base_type::end(), item, pair_sort_first_functor<T>());	}
-
-	inline bool     count(const T& key) const { return std::binary_search(base_type::begin(), base_type::end(), key); }
+	inline bool count(const T& key) const { return std::binary_search(base_type::begin(), base_type::end(), key); }
 
 	template<typename It>
-	void replace(It begin, It end) { base_type::replace(begin, end); sort(base_type::begin(), base_type::end());	}
-
-private:
-	template<class FwdIt>
-	static size_t compute_it_dist(FwdIt begin, FwdIt end)
-	{
-		size_t out = 0;
-		while(begin != end)
-		{
-			++out;
-			++begin;
-		}
-		return out;
-	}
+	void replace(It begin, It end) { base_type::replace(begin, end); std::sort(base_type::begin(), base_type::end());	}
 };
 
 #endif //LLVM_DATA_STRUCTURE_BENCHMARKS_ARRAYTYPES_H
